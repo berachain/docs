88f19cd0959364d85789be6fc1b545f25e607370
Larry the Cucumber <71284258+larrythecucumber321@users.noreply.github.com>
2025-01-08 06:23:45 -0800

    add toolbar to layout (#502)

--- apps/core/.vitepress/theme/index.ts
+++ apps/core/.vitepress/theme/index.ts
@@ -1,16 +1,21 @@
 // Imports
 // ========================================================
-// https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
+import { h, onMounted } from "vue";
 import DefaultTheme from "vitepress/theme";
 import Fidget from "@berachain/ui/Fidget";
 import "@berachain/ui/style";
+import { mountVercelToolbar } from "@vercel/toolbar/vite";
 
 // Theme Configuration
 // ========================================================
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    // Mount the Vercel toolbar when the component is mounted
+    onMounted(() => {
+      mountVercelToolbar();
+    });
+
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
       "sidebar-nav-after": () => h(Fidget),
