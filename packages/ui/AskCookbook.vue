bb805b0716c2bd89cabd541477aba47d09e1d95d
Ae0h <dontlisten33@gmail.com>
2025-06-16 14:56:07 -0700

    feat: 🎸 Cookbooks Chef AI integration

--- packages/ui/AskCookbook.vue
+++ packages/ui/AskCookbook.vue
@@ -9,14 +9,15 @@ const COOKBOOK_API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjE5N2EwN2U5OGJiYTgwM2ZiNGI0YzciLCJpYXQiOjE3MTI5NDU2NzEsImV4cCI6MjAyODUyMTY3MX0.Kcmgeb14z5-w0exHfo0W9rgS-wVhinFteCsPAMLQPoU";
 
 if (typeof document !== "undefined") {
-  let script = document.getElementById("__cookbook-script");
+  let script = document.getElementById("__cookbook-script") as HTMLScriptElement | null;;
   if (!script) {
-    script = document.createElement("script");
+    script = document.createElement("script") as HTMLScriptElement;
     script.src =
       "https://cdn.jsdelivr.net/npm/@cookbookdev/docsbot/dist/standalone/index.cjs.js";
     script.id = "__cookbook-script";
     script.async = true;
     document.body.appendChild(script);
+    console.log("🍳 Added Cookbook script" + document.body);
   }
 }
 </script>
@@ -26,5 +27,8 @@ Template
 ========================================================
 -->
 <template>
-  <div id="__cookbook" :data-api-key="COOKBOOK_API_KEY" />
+  <div
+    id="__cookbook"
+    :data-api-key="COOKBOOK_API_KEY"
+  ></div>
 </template>
